837355f4e8ff7afd75dff0c5a5d66e3c679ee2eb
biro224 <boubacarbirobarry837@gmail.com>
2024-03-22 16:03:06 +0300

    bill update

--- routes/bill.js
+++ routes/bill.js
@@ -41,4 +41,61 @@ router.post('/generateReport', auth.authenticateToken, (req, res) => {
     })
 })
 
+router.post('/getPdf',auth.authenticateToken,function(req,res){
+    const orderDetails = req.body;
+    const pdfPath = './generated_pdf'+orderDetails+'.pdf';
+    if(fs.existsSync(pdfPath)){
+        res.contentType("application/pdf");
+        fs.createReadStream(pdfPath).pipe(res);
+    }
+    else{
+        var productDetailsReport = JSON.parse(orderDetails.productDetails);
+        ejs.renderFile(path.join(__dirname, '', "report.ejs"), { productDetails: productDetailsReport, name: orderDetails.name, email: orderDetails.email, contactNumber: orderDetails.contactNumber, paymentMethod: orderDetails.paymentMethod, totalAmount: orderDetails.totalAmount }, (err, results) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+            else {
+                pdf.create(results).toFile('./generated_pdf/' + orderDetails.uuid + ".pdf", function (err, data) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(500).json(err);
+                    }
+                    else {
+                        res.contentType("application/pdf");
+                        fs.createReadStream(pdfPath).pipe(res);
+                    }
+                })
+            }
+        })
+    }
+})
+
+router.get('/getBills',auth.authenticateToken,(req,res,next)=>{
+    var query = "select* from bill order by id DESC";
+    connection.query(query,(err,results)=>{
+        if(!err){
+            return res.status(200).json(results);
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.delete('/delete/:id',auth.authenticateToken,(req,res,next)=>{
+    const id = req.params.id;
+    var query = "delete from bill where id=?";
+    connection.query(query,[id],(err,results)=>{
+        if(!err){
+            if(results.affectedRows == 0){
+                return res.status(404).json({message:"bill id does nos found"});
+            }
+            return res.status(200).json({message:"bill Deleted Successfully"});
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+})
+
 module.exports = router;
