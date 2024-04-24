58b4023bfb50313bbe87b5d43ee99aa91faaccfb
biro224 <boubacarbirobarry837@gmail.com>
2024-04-24 22:52:46 +0300

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
-require('dotenv').config();
+/*require('dotenv').config();
+
 const http = require('http');
 const app = require('./index');
 
 const server = http.createServer(app);
 server.listen(process.env.PORT);
+*/
+
+require('dotenv').config();
+
+const http = require('http');
+const app = require('./index');
+
+const port = process.env.PORT || 8080; // Use port 8080 as default if PORT is not defined
+
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
